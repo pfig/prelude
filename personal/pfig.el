627f953c3cd7b1bfb1781f2a66de6bd222af58d7
Pedro Figueiredo <pedro.figueiredo@fundingcircle.com>
2018-11-06 10:54:23 +0000

    Added user emacs directory explicitly

--- personal/pfig.el
+++ personal/pfig.el
@@ -1,3 +1,5 @@
 (setq user-full-name "Pedro Figueiredo"
       user-mail-address "pedro.figueiredo@fundingcircle.com"
       current-language-environment "English")
+
+(setq user-emacs-directory "~/.emacs.d")
